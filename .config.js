76fbb4cc46c8ee819286819c5315ae97e6ebd706
Ashik Miah <137385120+ritzpark@users.noreply.github.com>
2023-07-11 00:12:18 +0400

    Add files via upload

---
+++ .config.js
@@ -0,0 +1,11 @@
+// config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyBKWoH5uZQ-4xOMNfB7zCCsG-7QZPdrNFQ",
+    authDomain: "ritz-park.firebaseapp.com",
+    databaseURL: "https://ritz-park-default-rtdb.firebaseio.com",
+    projectId: "ritz-park",
+    storageBucket: "ritz-park.appspot.com",
+    messagingSenderId: "272741038168",
+    appId: "1:272741038168:web:d2434c2c88eefe7cd9f22d",
+    measurementId: "G-2W6BFG4FHT"
+};
